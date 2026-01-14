8805bcd5245a1ad197db4a7487701a7a5ce54d5a
Zainab Ajumobi <ajumobizainab@gmail.com>
2026-01-14 12:29:25 +0100

    updates web client import

--- src/main/java/com/zainab/PearsonBank/service/serviceimpl/ApiEmailServiceImpl.java
+++ src/main/java/com/zainab/PearsonBank/service/serviceimpl/ApiEmailServiceImpl.java
@@ -5,28 +5,21 @@ import com.zainab.PearsonBank.entity.FailedEmail;
 import com.zainab.PearsonBank.repository.FailedEmailRepository;
 import com.zainab.PearsonBank.service.EmailService;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.io.File;
-import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 @Slf4j
 @Profile("prod")
