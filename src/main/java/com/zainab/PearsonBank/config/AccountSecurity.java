19d532731490a4ebdaef88457772c31ab1c2f5db
Zainab Ajumobi <ajumobizainab@gmail.com>
2026-01-23 15:24:01 +0100

    autowires account repo into account security class

--- src/main/java/com/zainab/PearsonBank/config/AccountSecurity.java
+++ src/main/java/com/zainab/PearsonBank/config/AccountSecurity.java
@@ -1,13 +1,14 @@
 package com.zainab.PearsonBank.config;
 
 import com.zainab.PearsonBank.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component("accountSecurity")
 public class AccountSecurity {
+    @Autowired
     private AccountRepository accountRepository;
-    private Authentication authentication;
 
     public boolean accountBelongsToUser(Authentication authentication, String accountNumber) {
         String email = authentication.getName();
